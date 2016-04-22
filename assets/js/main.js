df2e6c2b25c71a1d5facb56bcf44ef561a70049f
Andrew Chen <moorheadandrew@gmail.com>
2016-04-22 03:21:20 -0700

    fixing scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,8 @@
 		var	$window = $(window),
 			$body = $('body'),
 			$header = $('#header'),
-			$banner = $('#banner');
+			$banner = $('#banner'),
+			$sidebar = $('#sidebar');
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
@@ -52,9 +53,91 @@
 		// Scrolly links.
 			$('.scrolly').scrolly({
 				speed: 1000,
-				offset: -1 
+				offset: function() {
+
+/*
+						if ($sidebar.length > 0 && !$header.hasClass('alt'))
+              console.log("HEAD:");
+							return $header.height();
+
+          console.log("-1");
+*/
+					return -1;
+
+				}
 			});
 
+		// Sidebar.
+			if ($sidebar.length > 0) {
+
+				var $sidebar_a = $sidebar.find('a');
+
+				$sidebar_a
+					.addClass('scrolly')
+					.on('click', function() {
+
+						var $this = $(this);
+
+						// External link? Bail.
+							if ($this.attr('href').charAt(0) != '#')
+								return;
+
+						// Deactivate all links.
+							$sidebar_a.removeClass('active');
+
+						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+							$this
+								.addClass('active')
+								.addClass('active-locked');
+
+					})
+					.each(function() {
+
+						var	$this = $(this),
+							id = $this.attr('href'),
+							$section = $(id);
+
+						// No section for this link? Bail.
+							if ($section.length < 1)
+								return;
+
+						// Scrollex.
+							$section.scrollex({
+								mode: 'middle',
+                /*
+								top: '-20vh',
+                */
+								bottom: '-20vh',
+								initialize: function() {
+
+									// Deactivate section.
+										if (skel.canUse('transition'))
+											$section.addClass('inactive');
+
+								},
+								enter: function() {
+
+									// Activate section.
+										$section.removeClass('inactive');
+
+									// No locked links? Deactivate all links and activate this section's one.
+										if ($sidebar_a.filter('.active-locked').length == 0) {
+
+											$sidebar_a.removeClass('active');
+											$this.addClass('active');
+
+										}
+
+									// Otherwise, if this section's link is the one that's locked, unlock it.
+										else if ($this.hasClass('active-locked'))
+											$this.removeClass('active-locked');
+
+								}
+							});
+
+					});
+
+			}
 		// Dropdowns.
 			$('#nav > ul').dropotron({
 				mode: 'fade',
