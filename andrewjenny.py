07c51d696883cf726748611e64c27252d6b91871
Andrew Chen <moorheadandrew@gmail.com>
2017-05-17 20:42:37 -0700

    added photo album urls

--- andrewjenny.py
+++ andrewjenny.py
@@ -9,24 +9,28 @@ import webapp2
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+  extensions=['jinja2.ext.autoescape'],
+  autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
+  def get(self):
+    template_values = {
+      'user': 'ANDREW CHENNNN',
+    }
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    self.response.write(template.render(template_values))
 
-    def get(self):
-        template_values = {
-            'user': 'ANDREW CHENNNN',
-        }
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
+class Photobooth(webapp2.RequestHandler):
+  def get(self):
+    self.redirect('https://goo.gl/photos/x7AEwUApJwzZDVQD6')
 
-class RSVP(webapp2.RequestHandler):
-  def post(self):
-    self.response.write(self.request)
+class Share(webapp2.RequestHandler):
+  def get(self):
+    self.redirect('https://goo.gl/photos/TZsPMiXo2vuWvRKk6')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/rsvphandler', RSVP),
+    ('/photobooth', Photobooth),
+    ('/share', Share)
 ], debug=True)
