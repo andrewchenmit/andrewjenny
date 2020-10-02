4ac28e631ddbfe295e8a6d6c20369611f33b0be7
Andrew Chen <moorheadandrew@gmail.com>
2020-10-01 22:49:54 -0700

    loader

--- pages/story/assets/js/login.js
+++ pages/story/assets/js/login.js
@@ -41,10 +41,8 @@
 							 'pages/story/images/banner16.jpg': 'center',
 							 'pages/story/images/banner17.jpg': 'center',
 							 'pages/story/images/banner19.jpg': 'center',
-							 'pages/story/images/banner21.jpg': 'center',
-							 'pages/story/images/banner20.jpg': 'center',
-							 'pages/story/images/banner18.jpg': 'center',
-							 'pages/story/images/banner24.jpg': 'bottom'
+							 'pages/story/images/banner20.png': 'center',
+							 'pages/story/images/banner18.jpg': 'center'
 						},
 
 					// Delay.
